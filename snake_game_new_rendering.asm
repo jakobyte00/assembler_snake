8234d2185d65b0ba448d5c6f079ea1a9e1b77934
Jakob Brodel <jakob.brodel@sap.com>
2026-05-02 14:03:14 +0200

    small errors + refactor

--- snake_game_new_rendering.asm
+++ snake_game_new_rendering.asm
@@ -16,8 +16,8 @@ Powx DATA 029H
 Pby DATA 030H
 Pbx DATA 031H
 ;Snake haed and tail indices
-Headidx DATA 032H
-Tailidx DATA 033H
+HeadIdx DATA 032H
+TailIdx DATA 033H
 ;Snake pixels ring buffer
 ;y-coordinates from 40H to 4FH
 ;x-coordinates from 50H to 5FH
@@ -58,76 +58,69 @@ MOV @R1, A
 RET
 
 MoveL:
-    ; --- 1. NEUEN KOPF BERECHNEN ---
-    ; Aktuellen Kopf aus dem Puffer auslesen
-    MOV A, #50H     ; Basis-Adresse X-Array
-    ADD A, HeadIdx  ; Aktuellen Kopf-Index addieren
-    MOV R0, A       ; R0 zeigt jetzt auf X-Wert des Kopfes
-    MOV A, @R0      ; A = Aktuelle X-Maske
-    MOV B, #002h
-    MUL AB          ; Multiplizieren = Verschiebung nach links
-    MOV Pbx, A      ; Neuen X-Wert in Pbx merken
+;Read current head y and x
+MOV A, #50H
+ADD A, HeadIdx
+MOV R0, A
+MOV A, @R0
+MOV B, #002h
+MUL AB
+MOV Pbx, A
 
-    MOV A, #40H     ; Basis-Adresse Y-Array
-    ADD A, HeadIdx
-    MOV R0, A       ; R0 zeigt jetzt auf Y-Wert des Kopfes
-    MOV A, @R0      ; A = Aktuelle Y-Adresse 
-    MOV Pby, A      ; Neuen Y-Wert in Pby merken (bleibt gleich bei MoveL)
+MOV A, #40H
+ADD A, HeadIdx
+MOV R0, A
+MOV A, @R0
+MOV Pby, A
 
-    ; --- 2. NEUEN KOPF IM PUFFER SPEICHERN ---
-    ; HeadIdx erhöhen (mit automatischem Überlauf bei 16)
-    MOV A, HeadIdx
-    INC A
-    ANL A, #0FH     ; Hält den Index streng zwischen 0 und 15 (Modulo 16)
-    MOV HeadIdx, A
+;Increment head index
+MOV A, HeadIdx
+INC A
+ANL A, #0FH     ; Hält den Index streng zwischen 0 und 15 (Modulo 16)
+MOV HeadIdx, A
 
-    ; Neue Werte in den Ringpuffer schreiben
-    MOV A, #50H
-    ADD A, HeadIdx
-    MOV R0, A
-    MOV A, Pbx
-    MOV @R0, A      ; X-Maske in neuen Head-Slot geschrieben
+;Add new head to screen
+MOV A, #50H
+ADD A, HeadIdx
+MOV R0, A
+MOV A, Pbx
+MOV @R0, A
 
-    MOV A, #40H
-    ADD A, HeadIdx
-    MOV R0, A
-    MOV A, Pby
-    MOV @R0, A      ; Y-Adresse in neuen Head-Slot geschrieben
+MOV A, #40H
+ADD A, HeadIdx
+MOV R0, A
+MOV A, Pby
+MOV @R0, A
 
-    ; --- 3. NEUEN KOPF ZEICHNEN ---
-    LCALL SetPixelFBuffer
+LCALL SetPixelFBuffer
 
-    ; --- 4. ALTEN SCHWANZ LÖSCHEN ---
-    ; Alte Schwanz-Koordinaten aus dem Puffer lesen
-    MOV A, #50H
-    ADD A, TailIdx
-    MOV R0, A
-    MOV A, @R0
-    MOV Pbx, A      ; Maske des Schwanzes
+;Delete old tail from screen
+MOV A, #50H
+ADD A, TailIdx
+MOV R0, A
+MOV A, @R0
+MOV Pbx, A
 
-    MOV A, #40H
-    ADD A, TailIdx
-    MOV R0, A
-    MOV A, @R0
-    MOV Pby, A      ; Y-Adresse des Schwanzes
+MOV A, #40H
+ADD A, TailIdx
+MOV R0, A
+MOV A, @R0
+MOV Pby, A
 
-    ; Schwanz vom Bildschirm löschen
-    LCALL DelPixelFBuffer
+LCALL DelPixelFBuffer
 
-    ; --- 5. SCHWANZ-POINTER WEITERSCHIEBEN ---
-    ; Der alte Schwanz ist gelöscht, das nächste Glied ist jetzt der Schwanz
-    MOV A, TailIdx
-    INC A
-    ANL A, #0FH     ; Wrap-around 0-15
-    MOV TailIdx, A
+;Move head
+MOV A, TailIdx
+INC A
+ANL A, #0FH     ; Wrap around 0-15
+MOV TailIdx, A
 
-    RET
+RET
 
 
 Main:
-LCALL SetPixelFBuffer
-LCALL DelPixelFBuffer
-SJMP Render
+LCALL MoveL
+LCALL Render
 SJMP Main
 
 Render:
@@ -135,7 +128,6 @@ Render:
 MOV Powx, #020h ;(Ze0)
 ;Put voltage on the first row
 MOV Powy, #080h
-SJMP RenderLoop
 
 RenderLoop:
 MOV R0, Powx
@@ -155,16 +147,9 @@ MOV B, #002h
 DIV AB
 MOV Powy, A
 
-;Check whether the last row has been rendered and we need to return to zero
-LCALL CheckLR
-LJMP RenderLoop
-
-CheckLR: ;Check if the last row has been reached and reset if true
+;Check if the last row has been reached and reset if true
 MOV A, Powx
-CJNE A, #028h, Return
-MOV Powy, #080h
-MOV Powx, #020h
-LCALL MoveL
+CJNE A, #028h, RenderLoop
 RET
 
 Return: ;Mapping function
